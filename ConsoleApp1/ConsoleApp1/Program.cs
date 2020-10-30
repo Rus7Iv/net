2a146a2552541c7aed3815656c3181c858b4d955
alex1666 <sany.kuchkarov.55@gmail.com>
2020-10-30 20:23:23 +0400

    prog1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.Json;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ведите количество дней которые вы прожили");
+            var days = Console.ReadLine();
+            int dayss= Convert.ToInt32(days);
+            int year = dayss/ 365;
+            int month = (dayss%365)/30;
+            dayss = (dayss % 365)%30;
+            Console.WriteLine($"Твой возраст {year} лет {month} месяцев и {dayss} дней");
+            if (year >= 18)
+            {
+                int a;
+                a = 0;
+                while (a!=10)
+                    { ++a;
+                    Console.WriteLine($"{a}");
+                }
+                Console.WriteLine($"Вы совершеннолетний!");
+
+
+            }
+            else
+                Console.WriteLine($"Вы несовершеннолетний");
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
